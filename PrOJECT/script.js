bf8e3dff308d74d9407fa6cb8e1e01e7b9ed8367
saurabhd542 <102911238+saurabhd542@users.noreply.github.com>
2024-07-31 20:36:38 +0530

    Add files via upload

---
+++ PrOJECT/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 80,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
